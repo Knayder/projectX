432095b92dad43ae5cb5652ff752751e0a6233d9
MadRussianHacker <m.gibas2407@gmail.com>
2019-03-17 20:02:25 +0100

    Added callback system for widgets

--- src/projectX/modules/gui/Widget.cpp
+++ src/projectX/modules/gui/Widget.cpp
@@ -1,7 +1,13 @@
 #include "Widget.hpp"
 #include "Container.hpp"
+#include <exception>
 
 namespace px {
+
+    Widget::Widget(const Container& parent):parent(parent){
+        for(size_t i=0; i<GuiEvent::COUNT; ++i)
+            callbacks.insert(std::pair<size_t, std::function<void()>>(i, [](){}) );
+    }
 
     void Widget::addRelativeOffset(sf::Vector2f offset){
         offset = offset/100.0f;
@@ -10,4 +16,45 @@ namespace px {
         this->move(offset);
     }
 
+    void Widget::bindCallback(size_t event, std::function<void()> callback) {
+        if( callbacks.find(event) == callbacks.end())
+            throw std::length_error("Invalid event value!");
+        callbacks.at(event) = callback;
+    }
+
+    void Widget::handleInput(const sf::Event& event){
+        static bool mouseOver = false;
+
+        if(event.type == sf::Event::MouseMoved){
+
+            if( isMouseOver( sf::Vector2f(event.mouseMove.x, event.mouseMove.y) ) ) {
+                callbacks.at(GuiEvent::ON_MOUSE_ENTER)();
+                mouseOver = true;
+            }
+            else {
+                callbacks.at(GuiEvent::ON_MOUSE_EXIT)();
+                mouseOver = false;
+            }
+
+        }
+
+        if(event.type == sf::Event::MouseButtonPressed){
+            if(mouseOver){
+
+                switch(event.mouseButton.button){
+                    case sf::Mouse::Left:
+                        callbacks.at(GuiEvent::LEFT_MB_PRESSED)();
+                    break;
+                    case sf::Mouse::Right:
+                        callbacks.at(GuiEvent::RIGHT_MB_PRESSED)();
+                    break;
+                    case sf::Mouse::Middle:
+                        callbacks.at(GuiEvent::MIDDLE_MB_PRESSED)();
+                    break;
+                }
+
+            }
+        }
+
+    }
 }
--- src/projectX/modules/gui/Widget.hpp
+++ src/projectX/modules/gui/Widget.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <functional>
+#include <map>
+
+#include "GuiEvents.hpp"
 
 namespace px {
     
@@ -9,7 +13,7 @@ namespace px {
     class Widget :public sf::Drawable, public sf::Transformable
     {
     public:
-        Widget(const Container& parent):parent(parent){}
+        Widget(const Container& parent);
         virtual ~Widget(){}
         
         //range of relative offset coordiantes: 0-100 [%] of the parent container size
@@ -17,7 +21,16 @@ namespace px {
 
         virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const=0;
 
+        void handleInput(const sf::Event& event);
+
+        void bindCallback(size_t event, std::function<void()> callback);
+
+    protected:
+        std::map< size_t, std::function<void()> > callbacks;    
+    
     private:
+        virtual bool isMouseOver(sf::Vector2f mousePosition) { return false; }
+
         const Container& parent;
     };
 
