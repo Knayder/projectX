fd7e4b20e44d01870d7d4d64c03c4ea4a5f304f2
MadRussianHacker <m.gibas2407@gmail.com>
2019-03-29 19:54:17 +0100

    Improved Container class

--- src/projectX/modules/gui/Container.cpp
+++ src/projectX/modules/gui/Container.cpp
@@ -1,6 +1,8 @@
 #include "Container.hpp"
 #include <exception>
 
+#include "projectX/utility/TupleForEach.hpp"
+
 namespace px {
 
     Container::Container(sf::Vector2f size, sf::Vector2f position)
@@ -9,22 +11,14 @@ namespace px {
     }
 
     void Container::draw(sf::RenderTarget& target, sf::RenderStates states) const {
-        for(const auto& widget : widgets){
-            target.draw(*(widget.second));
+        //forEach for tuple from utility doesn't work here
+        for(auto& widget : std::get<Child_t<Widget>>(childrenHolder)){
+            target.draw(*widget.second);
         }
-    }
 
-    void Container::addWidget(size_t hash, std::unique_ptr<Widget> widget) {
-        widget->setPosition(this->getPosition());
-        widgets.insert(std::pair<size_t, std::unique_ptr<Widget>>(hash, std::move(widget)));
-        widgets.at(hash)->setParent(this);
-    }
-
-    Widget& Container::getWidget(size_t hash) const {
-        if( widgets.find(hash) == widgets.end() )
-            throw std::length_error("Invalid widget id!");
-
-        return (*widgets.at(hash));
+        for(auto& container : std::get<Child_t<Container>>(childrenHolder)){
+            target.draw(*container.second);
+        }
     }
 
     sf::Vector2f Container::getSize() const {
@@ -32,9 +26,11 @@ namespace px {
     }
 
     void Container::handleInput(const sf::Event& event) {
-        for(const auto& widget : widgets){
-            widget.second->handleInput(event);
-        }
+        forEach(childrenHolder, [this, event](auto& children) {
+            for(auto& child : children){
+                child.second->handleInput(event);
+            }
+        });
     }
 
     void Container::setRelativeSize(sf::Vector2f size){
--- src/projectX/modules/gui/Container.hpp
+++ src/projectX/modules/gui/Container.hpp
@@ -5,11 +5,16 @@
 #include <map>
 
 #include "RelativeGuiObject.hpp"
+#include "projectX/utility/TypeTraits.hpp"
 
 namespace px {
 
     class Container :public sf::Drawable, public RelativeGuiObject
     {
+        template <typename ChildType>
+        using Child_t = std::map<size_t, std::unique_ptr<ChildType>>;
+    protected:
+        using ChildrenHolder_t = std::tuple<Child_t<Widget>, Child_t<Container>>;
     public:
         Container(sf::Vector2f size=sf::Vector2f(0.0f, 0.0f), sf::Vector2f position=sf::Vector2f(0.0f, 0.0f));
 
@@ -17,16 +22,32 @@ namespace px {
 
         void handleInput(const sf::Event& event);
 
-        void addWidget(size_t hash, std::unique_ptr<Widget> widget);
+        template <typename ChildType>
+        void addChild(size_t hash, std::unique_ptr<ChildType> child){
+            static_assert(tt::tuple_contains_type<Child_t<ChildType>, ChildrenHolder_t>(), "Container can't have this type of child!");
 
-        Widget& getWidget(size_t hash) const;
+            child->setPosition(this->getPosition());
+
+            std::get<Child_t<ChildType>>(childrenHolder).insert(std::pair<size_t, std::unique_ptr<ChildType>>(hash, std::move(child)));
+            std::get<Child_t<ChildType>>(childrenHolder).at(hash)->setParent(this);
+        }
+
+        template <typename ChildType>
+        ChildType& getChild(size_t hash) const {
+            static_assert(tt::tuple_contains_type<Child_t<ChildType>, ChildrenHolder_t>(), "Container doesn't have this type of child!");
+
+            if( std::get<Child_t<ChildType>>(childrenHolder).find(hash) == std::get<Child_t<ChildType>>(childrenHolder).end() )
+                throw std::length_error("Invalid child hash!");
+
+            return ( *std::get<Child_t<ChildType>>(childrenHolder).at(hash) );
+        }
 
         sf::Vector2f getSize() const;
 
         void setRelativeSize(sf::Vector2f size);
     private:
         sf::Vector2f size;
-        std::map<size_t, std::unique_ptr<Widget>> widgets;
+        ChildrenHolder_t childrenHolder;
     };
 
 }
